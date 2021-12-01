f92391b6e9ac6adbb9666102463467240af4f76a
nicolas.sandoval <nicolas.sandoval@rappi.com>
2021-11-30 22:14:16 -0300

    feature(KingRichardKnights): replace clone algorithm to a struct which is a buffer for the knights that are been moving

--- src/algorithms/hard/king_richard_knights.go
+++ src/algorithms/hard/king_richard_knights.go
@@ -1,6 +1,14 @@
 package hard
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type replace struct {
+	first  []uint64
+	second []uint64
+}
 
 // KingRichardKnights https://www.hackerrank.com/challenges/king-richards-knights/problem?h_r=next-challenge&h_v=zen
 // the first input the large of the array
@@ -10,20 +18,25 @@ import "fmt"
 // the second value is the first soldier to move
 // the third value is how many soldiers (starting from the second value) will move
 // knights that the king want to find after the moves
-func KingRichardKnights(n int32, commands [][]int32, knights []int32) [][]int32 {
+func KingRichardKnights(n uint64, commands [][]uint64, knights []uint64) [][]uint64 {
 	// create the knight formation
 	initialFormation := knightsArmy(n)
+	sort.Slice(knights, func(i, j int) bool {
+		return knights[i] < knights[j]
+	})
 	// this for go through the all commands
-	for move := 0; move < len(commands); move++ {
-		// clone the army
-		newFormation := cloneArmy(initialFormation)
-		command := commands[move]
+	for _, command := range commands {
 		line := command[0] - 1
 		initialLine := command[0] - 1
 		initialIndex := command[1] - 1
 		maxIndex := command[2] + command[1] - 1
 		// this is to include all cluster of knights that should be moved
-		for s := maxIndex - initialIndex; s >= 1; s = maxIndex - initialIndex {
+		for s := int64(maxIndex - initialIndex); s >= 1; s = int64(maxIndex - initialIndex) {
+			rep := replace{
+				first:  make([]uint64, s+1),
+				second: make([]uint64, s+1),
+			}
+			r := uint64(0)
 			// this is to do the 4 moves of a square
 			for i := 0; i < 4; i++ {
 				maxValue := maxIndex
@@ -32,19 +45,24 @@ func KingRichardKnights(n int32, commands [][]int32, knights []int32) [][]int32 
 				for j := initialIndex; j < maxIndex; j++ {
 					switch i {
 					case 0:
-						newFormation[line][maxIndex] = initialFormation[initialLine][j]
+						rep.first[r] = initialFormation[line][maxIndex]
+						initialFormation[line][maxIndex] = initialFormation[initialLine][j]
 					case 1:
-						newFormation[maxIndex-1][maxValue] = initialFormation[line][maxIndex]
+						rep.second[r] = initialFormation[maxIndex-1][maxValue]
+						initialFormation[maxIndex-1][maxValue] = rep.first[r]
 						maxValue--
 					case 2:
-						newFormation[maxValue-1][initialIndex] = initialFormation[maxIndex-1][maxValue]
+						rep.first[r] = initialFormation[maxValue-1][initialIndex]
+						initialFormation[maxValue-1][initialIndex] = rep.second[r]
 						maxValue--
 					case 3:
-						newFormation[initialLine][j] = initialFormation[maxValue-1][initialIndex]
+						initialFormation[initialLine][j] = rep.first[r]
 						maxValue--
 					}
+					r++
 					line++
 				}
+				r = 0
 				line = initialLine
 			}
 			initialIndex++
@@ -52,22 +70,20 @@ func KingRichardKnights(n int32, commands [][]int32, knights []int32) [][]int32 
 			line++
 			maxIndex--
 		}
-		initialFormation = newFormation
 	}
 	return searchValue(initialFormation, knights)
 }
 
-func searchValue(army [][]int32, knights []int32) [][]int32 {
-	result := make([][]int32, len(knights))
+func searchValue(army [][]uint64, knights []uint64) [][]uint64 {
+	result := make([][]uint64, len(knights))
 	for l, line := range army {
 		ki, li := 0, 0
 		for li < len(line) {
 			if ki > len(knights)-1 {
 				li++
 				ki = 0
-			} else if knights[ki] != -1 && line[li] == knights[ki] {
-				result[ki] = []int32{int32(l + 1), int32(li) + 1}
-				knights[ki] = -1
+			} else if line[li] == knights[ki] {
+				result[ki] = []uint64{uint64(l + 1), uint64(li) + 1}
 				ki++
 				continue
 			} else {
@@ -77,32 +93,34 @@ func searchValue(army [][]int32, knights []int32) [][]int32 {
 	}
 	return result
 }
-func knightsArmy(n int32) [][]int32 {
-	soldiers := make([][]int32, n)
-	var lastV int32
-	for i := int32(0); i < n; i++ {
-		line := make([]int32, n)
-		for j := int32(0); j < n; j++ {
+func knightsArmy(n uint64) [][]uint64 {
+	soldiers := make([][]uint64, n)
+	var lastV uint64
+	for i := uint64(0); i < n; i++ {
+		line := make([]uint64, n)
+		//go func(index int64) {
+		for j := uint64(0); j < n; j++ {
 			line[j] = lastV
 			lastV++
 		}
 		soldiers[i] = line
+		//}(i)
 	}
 	return soldiers
 }
 
-func cloneArmy(a [][]int32) [][]int32 {
+func cloneArmy(a [][]uint64) [][]uint64 {
 	l := len(a)
-	r := make([][]int32, l)
+	r := make([][]uint64, l)
 	for i := 0; i < l; i++ {
-		newA := make([]int32, len(a[i]))
+		newA := make([]uint64, len(a[i]))
 		copy(newA, a[i])
 		r[i] = newA
 	}
 	return r
 }
 
-func printFormation(l [][]int32) {
+func printFormation(l [][]uint64) {
 	for j := 0; j < len(l); j++ {
 		fmt.Println(l[j])
 	}
--- src/algorithms/hard/king_richard_knights_test.go
+++ src/algorithms/hard/king_richard_knights_test.go
@@ -5,23 +5,23 @@ import (
 )
 
 func TestKinRichardKnights(t *testing.T) {
-	t.Run("should return a two dimenssion array", func(t *testing.T) {
-		result := KingRichardKnights(int32(7), [][]int32{
+	t.Run("should return a two dimension array", func(t *testing.T) {
+		result := KingRichardKnights(uint64(7), [][]uint64{
 			{1, 2, 4},
 			{2, 3, 3},
 			{3, 4, 1},
 			{3, 4, 0},
-		}, []int32{0,
-			6,
+		}, []uint64{0,
 			9,
+			6,
 			11,
-			24,
+			48,
 			25,
-			48})
-		if result[0][1] != int32(1) {
+			24})
+		if result[0][1] != uint64(1) {
 			t.Errorf("should return 1 and is %v", result[0][1])
 		}
-		if result[5][1] != int32(4) {
+		if result[5][1] != uint64(4) {
 			t.Errorf("should return 1 and is %v", result[0][1])
 		}
 	})
