ef26a436a1d247c0e1033af3b6124c46a70419dc
nicolas.sandoval <nicolas.sandoval@rappi.com>
2022-05-24 21:53:27 -0400

    medium(find-in-rotate-array): find a value in a rotate arrya in a log n complexity.

--- src/algorithms/medium/find_in_rotate_array.go
+++ src/algorithms/medium/find_in_rotate_array.go
@@ -1,6 +1,6 @@
 package medium
 
-func FindInRotateArray(arr []int, l, r, v int) int {
+func FindInRotateArrayRecursive(arr []int, l, r, v int) int {
 	m := l + (r-l)/2
 
 	if m <= len(arr)-1 {
@@ -9,18 +9,44 @@ func FindInRotateArray(arr []int, l, r, v int) int {
 		}
 
 		if arr[l] > arr[m] && (arr[l] <= v && arr[m] < v) {
-			return FindInRotateArray(arr, l, m-1, v)
+			return FindInRotateArrayRecursive(arr, l, m-1, v)
 		}
 
 		if arr[r] < arr[m] && (arr[m] > v && arr[r] <= v) {
-			return FindInRotateArray(arr, m+1, r, v)
+			return FindInRotateArrayRecursive(arr, m+1, r, v)
 		}
 
 		if arr[m] > v {
-			return FindInRotateArray(arr, l, m-1, v)
+			return FindInRotateArrayRecursive(arr, l, m-1, v)
 		}
 
-		return FindInRotateArray(arr, m+1, r, v)
+		return FindInRotateArrayRecursive(arr, m+1, r, v)
+	}
+
+	return -1
+}
+
+func FindInRotateArrayLoop(arr []int, l, r, v int) int {
+
+	for l < r {
+		m := l + (r-l)/2
+		if arr[m] == v {
+			return m
+		}
+
+		if arr[l] > arr[m] && (arr[l] <= v && arr[m] < v) {
+			r = m - 1
+		}
+
+		if arr[r] < arr[m] && (arr[m] > v && arr[r] <= v) {
+			l = m + 1
+		}
+
+		if arr[m] > v {
+			r = m - 1
+		}
+
+		l = m + 1
 	}
 
 	return -1
