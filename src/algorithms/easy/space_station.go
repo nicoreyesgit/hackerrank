d7ffeb86a6ae22a364349542ddae6d8e79f49384
nicolas.sandoval <nicolas.sandoval@rappi.com>
2022-05-30 22:09:48 -0400

    easy: add flat space station solution.
    It took me more than i expected. Continue studing!

--- src/algorithms/easy/space_station.go
+++ src/algorithms/easy/space_station.go
@@ -1,6 +1,9 @@
 package easy
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 // https://www.hackerrank.com/challenges/flatland-space-stations/problem?h_r=next-challenge&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 func flatlandSpaceStations(n int32, c []int32) int32 {
@@ -8,39 +11,50 @@ func flatlandSpaceStations(n int32, c []int32) int32 {
 		return c[i] < c[j]
 	})
 
-	var maxDistance, topDistance, distance, city, i, j int32
+	var maxDistance, station, city, si, bDistance, fDistance, bestDistance int32
+	lastStation := c[0]
+	station = lastStation
 
-	for city < n-1 {
-		i = city
-
-		for j < int32(len(c)) {
-			if i == c[j] {
-				if city == c[j] {
-					j++
-				}
-
-				if distance > maxDistance {
-					maxDistance = distance
-				}
-
-				distance = 0
-				city++
-
-				break
+	if len(c) == 1 {
+		if n > 1 {
+			if c[0] > 0 {
+				return c[0]
 			}
-
-			distance++
-			i++
-		}
-
-		if j == int32(len(c)) && city > c[j-1] {
-			topDistance = n - c[j-1] + 1
-			break
+			return n - c[0] - 1
+		} else {
+			return 0
 		}
 	}
 
-	if topDistance > maxDistance {
-		return topDistance
+	for city < n {
+		if station == city {
+			if si < int32(len(c)) {
+				lastStation = c[si]
+
+				si++
+				if si <= int32(len(c))-1 && c[si] < n {
+					station = c[si]
+				}
+			}
+
+			city++
+
+			continue
+		}
+
+		bDistance = int32(math.Abs(float64(city - lastStation)))
+		fDistance = int32(math.Abs(float64(station - city)))
+		if bDistance < fDistance {
+			bestDistance = bDistance
+		} else {
+			bestDistance = fDistance
+		}
+
+		if bestDistance > maxDistance {
+			maxDistance = bestDistance
+		}
+
+		city++
 	}
 
 	return maxDistance
--- src/algorithms/easy/space_station_test.go
+++ src/algorithms/easy/space_station_test.go
@@ -3,10 +3,10 @@ package easy
 import "testing"
 
 func TestFlatlandSpaceStations(t *testing.T) {
-	stations := []int32{0, 4}
-	r := flatlandSpaceStations(5, stations)
+	stations := []int32{99}
+	r := flatlandSpaceStations(100, stations)
 
-	if r != 2 {
+	if r != 20 {
 		t.Error("should be 0 and is ", r)
 	}
 }
